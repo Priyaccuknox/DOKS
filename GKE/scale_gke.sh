d6a817f47e0a8bffb10de3fd1a531589dc15cc4b
Priya <priya@accuknox.com>
2024-10-28 18:59:11 +0530

    gke updated

--- GKE/scale_gke.sh
+++ GKE/scale_gke.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Load required variables from ./GKE/config.yaml
-clusters=$(yq '.clusters | length' ./GKE/config.yaml)
+# Load required variables
+clusters=$(yq '.clusters | length' config.yaml)
 
 # Check if the operation is passed (scale-up or scale-down)
 operation=$1
@@ -10,25 +10,28 @@ if [[ "$operation" != "scale-up" && "$operation" != "scale-down" ]]; then
   exit 1
 fi
 
+# Activate GCP service account
+gcloud auth activate-service-account --key-file="$GCP_CREDENTIALS"
+
 # Perform the scaling operation for each cluster
 for i in $(seq 0 $(($clusters - 1))); do
-  CLUSTER_NAME=$(yq ".clusters[$i].name" ./GKE/config.yaml)
-  CLUSTER_ID=$(yq ".clusters[$i].cluster_id" ./GKE/config.yaml)
-  NODE_POOL_ID=$(yq ".clusters[$i].node_pool_id" ./GKE/config.yaml)
+  CLUSTER_NAME=$(yq ".clusters[$i].name" config.yaml)
+  CLUSTER_ID=$(yq ".clusters[$i].cluster_id" config.yaml)
+  NODE_POOL_ID=$(yq ".clusters[$i].node_pool_id" config.yaml)
 
   if [ "$operation" == "scale-up" ]; then
-    NODE_COUNT=$(yq ".clusters[$i].scale_up_count" ./GKE/config.yaml)
+    NODE_COUNT=$(yq ".clusters[$i].scale_up_count" config.yaml)
   else
-    NODE_COUNT=$(yq ".clusters[$i].scale_down_count" ./GKE/config.yaml)
+    NODE_COUNT=$(yq ".clusters[$i].scale_down_count" config.yaml)
   fi
 
   echo "Performing $operation for cluster $CLUSTER_NAME..."
   echo "Setting node pool count to $NODE_COUNT..."
 
-  # Perform the scaling operation
-  gcloud container clusters resize $CLUSTER_NAME \
-    --node-pool $NODE_POOL_ID \
-    --num-nodes $NODE_COUNT \
+  # Perform the scaling operation using gcloud command
+  gcloud container clusters resize "$CLUSTER_ID" \
+    --node-pool "$NODE_POOL_ID" \
+    --num-nodes "$NODE_COUNT" \
     --quiet
 
   echo "$operation completed for cluster $CLUSTER_NAME."
